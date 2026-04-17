92f28331edafb9e2b43e9fc770327c3995a8ee39
Ataxanov <asherzod2197@gmail.com>
2026-04-17 16:50:03 +0500

    Add string manipulation examples in main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# 26
+s = "Men Python o'rganaman"
+print(len(s.split()))
+
+# 27
+s = "python"
+print(s[0].upper() + s[-1].upper())
+
+# 28
+s = "Men Python o'rganaman"
+print(s.replace(" ", ""))
+
+# 29
+s = "salom salom dunyo"
+print(s.replace("salom", "hi", 1))
+
+# 30
+s = "salom dunyo salom"
+print(s.rindex("salom"))
